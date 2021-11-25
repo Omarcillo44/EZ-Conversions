d9c8856823f301316c926e08ace1dd84f0f0d74c
Omar Lorenzo Pacheco <omarlorenz44@gmail.com>
2021-11-25 01:10:05 -0600

    Recuperación de repositorio

--- app/src/main/java/ipn/cecyt9/ezconversions/MainActivity.java
+++ app/src/main/java/ipn/cecyt9/ezconversions/MainActivity.java
@@ -21,12 +21,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonPesosDolar = findViewById(R.id.ButtonPesosDolar);
-
+        buttonOnzas = findViewById(R.id.button_onzas);
+        buttonCirculo = findViewById(R.id.button_circulo);
+        buttonWatts = findViewById(R.id.button_watts);
 
         buttonPesosDolar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 abrirPesosDolar();
+            }
+        });
+
+        buttonOnzas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirOnzas();
+            }
+        });
+
+        buttonCirculo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirCirculo();
+            }
+        });
+
+        buttonWatts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirWatts();
             }
         });
 
@@ -36,6 +59,21 @@ public class MainActivity extends AppCompatActivity {
 
     private void abrirPesosDolar() {
         Intent intent = new Intent(this , PesosDolar.class);
+        startActivity(intent);
+    }
+
+    private void abrirOnzas() {
+        Intent intent = new Intent(this , Onzas.class);
+        startActivity(intent);
+    }
+
+    private void abrirCirculo() {
+        Intent intent = new Intent(this , Circulo.class);
+        startActivity(intent);
+    }
+
+    private void abrirWatts() {
+        Intent intent = new Intent(this , Watts.class);
         startActivity(intent);
     }
 
---
+++ app/src/main/java/ipn/cecyt9/ezconversions/Onzas.java
@@ -0,0 +1,14 @@
+package ipn.cecyt9.ezconversions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Onzas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onzas);
+    }
+}
